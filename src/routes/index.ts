b2af79ea725e0191f81b4268c9124e4a7064e107
Daniel Cavalcanti <dpc2@cin.ufpe.br>
2021-08-24 20:13:09 -0300

    setup project
    Signed-off-by: Daniel Cavalcanti <dpc2@cin.ufpe.br>

---
+++ src/routes/index.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+// import UsersController from "../controllers/UsersController";
+// import PostsController from "../controllers/PostsController";
+// import CompaniesController from "../controllers/CompaniesController";
+// import UserPostsController from "../controllers/UserPostsController";
+
+const routes = Router();
+
+// routes.get("/users", UsersController.index);
+// routes.get("/users/:id", UsersController.show);
+// routes.get("/users/:id/posts", UserPostsController.index);
+
+// routes.get("/companies", CompaniesController.index);
+// routes.get("/companies/:slug", CompaniesController.show);
+
+// routes.get("/posts", PostsController.index);
+// routes.get("/posts/:id", PostsController.show);
+
+export default routes;
